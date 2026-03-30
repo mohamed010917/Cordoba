e7e2b5f5e74c45b23f3691af9277d6c7269a490f
Abdulhamid Gouda <goudaabdulhmid@gmail.com>
2026-03-30 18:36:19 +0200

    Refactor StoreFloorRequest and UpdateFloorRequest classes for authorization and validation rules

--- app/Http/Requests/StorefloorRequest.php
+++ app/Http/Requests/StorefloorRequest.php
@@ -2,28 +2,19 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class StorefloorRequest extends FormRequest
+class StoreFloorRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return $this->user()->can('create', \App\Models\Floor::class);
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'min:3'],
         ];
     }
 }
--- app/Http/Requests/UpdatefloorRequest.php
+++ app/Http/Requests/UpdatefloorRequest.php
@@ -2,28 +2,19 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdatefloorRequest extends FormRequest
+class UpdateFloorRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return $this->user()->can('update', $this->route('floor'));
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'min:3'],
         ];
     }
 }
